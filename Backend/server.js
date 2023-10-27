2da8752493382e1e930faecb4447cb544278df33
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-10-27 16:24:11 +0530

    refractor:

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,24 @@
-const http = require('http');
-require('dotenv').config(); 
+const http = require("http");
+require("dotenv").config();
 
 const apiKey = process.env.API_KEY;
 
 if (!apiKey) {
-  console.error("API key is not set. Please set it as an environment variable.");
+  console.error(
+    "API key is not set. Please set it as an environment variable."
+  );
   process.exit(1);
 }
 
-
-
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Node.js Server!');
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, Node.js Server!");
 });
 
 const port = 3000;
 server.listen(port, () => {
-    console.log("-------------------------");
-    console.log(`API key is ${apiKey}`);
-    console.log(`Server is running on http://localhost:${port}`);
-    console.log("-------------------------")
+  console.log("-------------------------");
+  console.log(`API key is ${apiKey}`);
+  console.log(`Server is running on http://localhost:${port}`);
+  console.log("-------------------------");
 });
-
-
